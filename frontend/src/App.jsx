41b03acc5a72003bac9dbd7cdd9d1fee02c72ea5
Yathartha19 <aarushyathartha19@gmail.com>
2024-09-13 09:53:31 +0530

    added logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import Url from './components/Url'
 import './main.css'
+import logo from './assets/logo.png'
 
 function App() {
 
   return (
     <>
-      <img src='./src/public/logo.png' className='absolute h-20 top-5 left-1/2 transform -translate-x-1/2'/>
+      <img src={logo} className='absolute h-20 top-5 left-1/2 transform -translate-x-1/2'/>
       <Url />
     </>
   )
